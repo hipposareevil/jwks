f026f96f5a0e11b34463938e4d54844359d6ab80
Samuel Jackson <samuel.jackson@salesforce.com>
2020-10-27 08:16:33 -0700

    reset

--- src/main/java/com/wpff/jwks/Application.java
+++ src/main/java/com/wpff/jwks/Application.java
@@ -11,8 +11,10 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
+
+
 	}
 
 }
